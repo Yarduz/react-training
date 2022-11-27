24ce782ddded1e648eaffaa79ef016e15cfd4c21
Yarduz <106406084+Yarduz@users.noreply.github.com>
2022-11-28 01:31:59 +0200

    it finally works!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import './App.css';
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+
+const initialState = {
+  bill: 100,
+  tip: 0.1,
+  dinners: 2
+}
 
 function App() {
-
-  const initialState = {
-    bill: 0,
-    tip: 0,
-    dinners: 0
-  }
   const [state, setState] = useState(initialState);
+  const { bill, dinners, tip } = state
 
-  const totalBillPP = (state.bill + state.tip) / state.dinners
-  const tipBillPP = state.tip / state.dinners
+  const totalBillPP = (bill + (bill * tip)) / dinners;
+  const tipBillPP = bill * tip / dinners;
+  const displaytotal = dinners ? totalBillPP : 0
+  const displayTip = dinners ? tipBillPP : 0
 
   const reset = () => setState(initialState);
 
+  function changeInDinners(event) {
+    console.log(state)
+    setState({ ...state, dinners: event.target.value })
+  }
+  function changeInBill(event) {
+    console.log(state)
+    setState({ ...state, bill: event.target.value })
+  }
+  function changeInTip(event) {
+    console.log(state)
+    setState({ ...state, tip: event.target.value })
+  }
 
   return (
     <div className="App d-flex justify-content-center flex-column">
@@ -27,26 +41,26 @@ function App() {
         <div className='darkerGreen calc m-2 p-2'>
           <div className='bill mx-5 d-flex flex-column'>
             <label htmlFor="" className='align-self-start'>bill</label>
-            <input dir="rtl" id='inputBill' type="number" step="0.01" min={0} />
+            <input onChange={changeInBill} dir="rtl" id='inputBill' type="number" step="0.01" min={1} />
           </div>
           <div className='bill mx-5 d-flex flex-column my-2'>
             <label htmlFor="" className='align-self-start my-2'>select tip %</label>
             <div className='buttonArea d-flex flex-column my-2'>
               <div className='d-flex flex-row gap-2 my-2'>
-                <button className='py-3 col-4 rounded tipB darkerGreenbg tipB'>5%</button>
-                <button className='py-3 col-4 rounded tipB darkerGreenbg tipB'>10%</button>
-                <button className='py-3 col-4 rounded tipB darkerGreenbg tipB'>15%</button>
+                <button value={0.05} onClick={changeInTip} className='py-3 col-4 rounded tipB darkerGreenbg tipB'>5%</button>
+                <button value={0.1} onClick={changeInTip} className='py-3 col-4 rounded tipB darkerGreenbg tipB'>10%</button>
+                <button value={0.15} onClick={changeInTip} className='py-3 col-4 rounded tipB darkerGreenbg tipB'>15%</button>
               </div>
               <div className='d-flex flex-row gap-2 my-2 tipB'>
-                <button className='py-3 col-4 rounded tipB darkerGreenbg tipB'>25%</button>
-                <button className='py-3 col-4 rounded tipB darkerGreenbg tipB'>50%</button>
-                <button id='inputPeople' className='py-3 col-4 rounded tipBC'>CUSTOM<input id='customInput' type="number" min={0} /></button>
+                <button value={0.25} onClick={changeInTip} className='py-3 col-4 rounded tipB darkerGreenbg tipB'>25%</button>
+                <button value={0.5} onClick={changeInTip} className='py-3 col-4 rounded tipB darkerGreenbg tipB'>50%</button>
+                <button id='inputPeople' className='py-3 col-4 rounded tipBC'>CUSTOM<input onChange={changeInTip} id='customInput' type="number" min={1} /></button>
               </div>
             </div>
           </div>
           <div className='bill mx-5 d-flex flex-column'>
             <label htmlFor="" className='align-self-start'>number of people</label>
-            <input dir="rtl" id='inputPeople' placeholder='1' type="number" align="right" step="1" min={2} />
+            <input onChange={changeInDinners} dir="rtl" placeholder='2' id='inputPeople' type="number" align="right" step="1" />
           </div>
         </div>
         <div className='amount darkerGreenbg m-2 p-2 rounded'>
@@ -55,12 +69,12 @@ function App() {
               <h4>tip amount</h4>
               <p className='ms-1'>/ person</p>
             </div>
-            <h5 id='tipAmount' className='col-6'>{tipBillPP}</h5>
+            <h5 id='tipAmount' className='col-6' >{displayTip}</h5>
             <div className='col-5 ms-2'>
               <h4>total</h4>
               <p className='ms-1'>/ person</p>
             </div>
-            <h5 id='totalAmount' className='col-6'>{totalBillPP}</h5>
+            <h5 id='totalAmount' className='col-6'>{displaytotal}</h5>
           </div>
           <button id='reset' onClick={reset} className='py-2 rounded align-self-strech'>RESET</button>
         </div>
